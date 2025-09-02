60a29c129d0a5b887e089d92f302120767bedece
kartik1500 <kartikgarg95883@gmail.com>
2025-09-02 21:17:01 +0530

    final

--- client/src/components/RightSidebar.jsx
+++ client/src/components/RightSidebar.jsx
@@ -1,7 +1,23 @@
-import React from "react";
-import assets, { imagesDummyData } from "../assets/assets";
+import React, { useContext, useEffect, useState } from "react";
+import assets from "../assets/assets";
+import { ChatContext } from "../../context/ChatContext.js";
+import { AuthContext } from "../../context/AuthContext";
 
-const RightSidebar = ({ selectedUser }) => {
+const RightSidebar = ({}) => {
+  const { selectedUser, messages } = useContext(ChatContext);
+  const { logout, onlineUsers } = useContext(AuthContext);
+  const [msgImages, setMsgImages] = useState([]);
+
+  // Get all images from the messages and set them to state
+
+  // useEffect(() => {
+  //   setMsgImages(messages.filter((msg) => msg.type === "image").map((msg) => msg.content));
+  // }, [messages]);
+
+  useEffect(() => {
+    setMsgImages(messages.filter((msg) => msg.image).map((msg) => msg.image));
+  }, [messages]);
+
   return (
     selectedUser && (
       <div
@@ -17,9 +33,13 @@ const RightSidebar = ({ selectedUser }) => {
           />
           <h1
             className="px-10 text-xl font-medium mx-auto flex items-center
-gap-2"
+            gap-2"
           >
-            <p className="w-2 h-2 rounded-full bg-green-500"></p>
+            {onlineUsers.includes(selectedUser._id) ? (
+              <p className="w-2 h-2 rounded-full bg-green-500"></p>
+            ) : (
+              <p className="w-2 h-2 rounded-full bg-red-500"></p>
+            )}
             {selectedUser.fullName}
           </h1>{" "}
           <p className="px-10 mx-auto">{selectedUser.bio}</p>
@@ -32,14 +52,17 @@ gap-2"
             className="mt-2 max-h-[200px] overflow-y-scroll grid grid-cols-2
 gap-4 opacity-80"
           >
-            {imagesDummyData.map((url, index) => (
+            {msgImages.map((url, index) => (
               <div key={index} onClick={() => window.open(url)} className="cursor-pointer rounded">
                 <img src={url} alt="" className="h-full rounded-md" />
               </div>
             ))}
           </div>
         </div>
-        <button className="absolute bottom-5 left-1/2 transform -translate-x-1/2 bg-gradient-to-r from-purple-400 to-violet-600 text-white border-none text-sm font-light py-2 px-20 rounded-full cursor-pointer">
+        <button
+          onClick={() => logout()}
+          className="absolute bottom-5 left-1/2 transform -translate-x-1/2 bg-gradient-to-r from-purple-400 to-violet-600 text-white border-none text-sm font-light py-2 px-20 rounded-full cursor-pointer"
+        >
           Logout
         </button>
       </div>
--- client/src/pages/Homepage.jsx
+++ client/src/pages/Homepage.jsx
@@ -1,10 +1,11 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import Sidebar from "../components/Sidebar";
 import ChatContainer from "../components/ChatContainer";
 import RightSidebar from "../components/RightSidebar";
+import { ChatContext } from "../../context/ChatContext.js";
 
 const Homepage = () => {
-  const [selectedUser, setSelectedUser] = useState(false);
+  const { selectedUser, setSelectedUser } = useContext(ChatContext);
 
   return (
     <div className="border w-full h-screen sm:px-[15%] sm:py-[5%] ">
@@ -17,7 +18,7 @@ const Homepage = () => {
       >
         <Sidebar />
         <ChatContainer />
-        <RightSidebar selectedUser={selectedUser} setSelectedUser={setSelectedUser} />
+        <RightSidebar />
       </div>
     </div>
   );
